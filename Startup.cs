04d6f8890ecf35e6258293ad4c17853bc88868fb
Sam Attwood <59017719+samattwood9@users.noreply.github.com>
2023-11-20 09:16:41 +0000

    Remove unused usings

--- Startup.cs
+++ Startup.cs
@@ -3,16 +3,12 @@ using api.ErrorHandling;
 using api.Models;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using Npgsql;
 using SimpleInjector;
-using System;
-using System.Linq;
 
 namespace api
 {
