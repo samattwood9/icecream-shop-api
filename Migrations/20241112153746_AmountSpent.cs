e9083791c4763353c1a59789607ac5cfacf6cd61
Sam Attwood <59017719+samattwood9@users.noreply.github.com>
2024-11-12 15:40:49 +0000

    Fix for move to dotnet version 8

---
+++ Migrations/20241112153746_AmountSpent.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class AmountSpent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "AmountSpent",
+                table: "Customers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: 0m);
+
+            migrationBuilder.CreateTable(
+                name: "Toppings",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Name = table.Column<string>(type: "TEXT", nullable: true),
+                    Price = table.Column<decimal>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Toppings", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Toppings");
+
+            migrationBuilder.DropColumn(
+                name: "AmountSpent",
+                table: "Customers");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -23,7 +23,7 @@ dotnet restore
 If you don't have the EF core tools, run:
 
 ```c#
-dotnet tool install -g dotnet-ef --version 7.0.0
+dotnet tool install dotnet-ef
 ```
 
 Then (or straightaway if you already have the tools), run:
@@ -45,8 +45,3 @@ dotnet run
 Navigate to http://localhost:5001 to view the Swagger (interactive documentation for the API).
 
 Use the Seed endpoint to seed the database and then experiment with the different endpoints.
-
-## Notes
-
-- This should work with net7.0. If you have multiple versions installed set the version in a global.json file. See: https://learn.microsoft.com/en-us/dotnet/core/versions/selection. You can check your sdk versions with: `dotnet --list-sdks`.
-- https://jasonwatmore.com/post/2022/09/05/net-6-connect-to-sqlite-database-with-entity-framework-core (might be helpful if there are issues getting the db set-up, note you might need to specify versions for the nugetpackages, E.g., `--version 7.0.0`).
